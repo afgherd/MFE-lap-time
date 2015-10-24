48916f76851acceac832a90ccd840ec913abc3ec
MFE-suspension <schyleeric@hotmail.com>
2015-10-24 15:17:52 -0400

    Working files
    Added motor model

--- Operations/Working files/f_lap_start.m
+++ Operations/Working files/f_lap_start.m
@@ -2,21 +2,29 @@ function [ track ] = f_lap_start( track,car,accel_eqn,vf_eqn )
 % Start lap at v0=0
 
 % initialize v_max at max speed
-% (take minimum of velocity and acceleration profile)
+% (in order to take minimum of velocity and acceleration profile)
 v_max(1:size(track.results.v_profile,1),1)=car.motor_speed;
 
 % Start lap, initial velocity=0
 v_max(1,1)=0;
 i=1;
 while v_max(i,1)<=track.results.v_profile(i)
-    acceleration=accel_eqn(abs(track.r(i+1)),v_max(i,1));
-    if isreal(acceleration)
-        accel_tire(i,1)=acceleration;
+    
+    % find max acceleration allowed by motor
+    [force,rpm]=f_motor_map(v_max(i,1));
+    track.results.accel_motor(i,1)=force/car.mass;
+    track.results.rpm(i,1)=rpm;
+    % find max acceleration allowed by tires
+    accel_traction=accel_eqn(abs(track.r(i+1)),v_max(i,1));
+    if isreal(accel_traction)
+        track.results.accel_tire(i,1)=accel_traction;
     else
         break
     end
-    
-    vf=vf_eqn(accel_tire(i,1),v_max(i,1),abs(track.r(i+1)));
+    % find max acceleration
+    track.results.accel_max(i,1)=min(track.results.accel_motor(i,1),track.results.accel_tire(i,1));
+
+    vf=vf_eqn(track.results.accel_max(i,1),v_max(i,1),abs(track.r(i+1)));
     v_max(i+1,1)=vf(vf>0);
     i=i+1;
 end
